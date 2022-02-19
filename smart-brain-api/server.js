47d25edbf4a99c09893c6eb42509a913e44eae06
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2022-02-19 18:47:57 +0200

    Set up the server

---
+++ smart-brain-api/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: 'john"gmail.com',
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("signin");
+  } else {
+    res.status(400).json("error login in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = new user
+/profile/:userId --> GET = user
+/image --> PUT = updated user object
+*/
